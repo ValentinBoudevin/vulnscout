64c105d5dbaed213758353756e969db214ed181a
Youenn Le Jeune <youenn.lejeune@savoirfairelinux.com>
2026-05-12 14:36:36 +0200

    test: fix frontend tests
    Signed-off-by: Youenn Le Jeune <youenn.lejeune@savoirfairelinux.com>

--- frontend/src/handlers/vulnerabilities.ts
+++ frontend/src/handlers/vulnerabilities.ts
@@ -42,7 +42,7 @@ type Vulnerability = {
     texts: {
         title: string;
         content: string;
-        packages: string[];
+        packages?: string[];
     }[];
     severity: {
         severity: string;
