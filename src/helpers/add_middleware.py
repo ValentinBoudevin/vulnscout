ce5d8184bf3872525dc168c6ed9f7556b5ef903e
Youenn Le Jeune <youenn.lejeune@savoirfairelinux.com>
2026-06-11 16:41:26 +0200

    types: correctly annotate middlewares
    Signed-off-by: Youenn Le Jeune <youenn.lejeune@savoirfairelinux.com>

--- src/helpers/add_middleware.py
+++ src/helpers/add_middleware.py
@@ -4,27 +4,30 @@
 from typing import Any, Callable
 
 from flask import Flask
+from flask.typing import RouteCallable, ResponseReturnValue
 from functools import wraps
 
 # custom definition for @app.middleware. Use it as you would @app.route
 # If you return something, the request will be stopped and the return value will be sent to the client
 # If you return None, the request will continue to the next middleware or route
 
+MiddlewareCallable = Callable[..., ResponseReturnValue | None]
+
 
 class FlaskWithMiddleware(Flask):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
 
-        self.middlewares: list[tuple[str, Callable[..., Any]]] = []
+        self.middlewares: list[tuple[str, MiddlewareCallable]] = []
         super().__init__(*args, **kwargs)
 
-    def middleware(self, path_prefix: str) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
-        def middleware_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+    def middleware(self, path_prefix: str) -> Callable[[MiddlewareCallable], MiddlewareCallable]:
+        def middleware_decorator(func: MiddlewareCallable) -> MiddlewareCallable:
             self.middlewares.append((path_prefix.lstrip("/"), func))
             return func
         return middleware_decorator
 
-    def route(self, rule: str, **options: Any) -> Callable[[Callable[..., Any]], Any]:  # type: ignore[override]
-        def route_decorator(func: Callable[..., Any]) -> Any:
+    def route(self, rule: str, **options: Any) -> Callable[[RouteCallable], RouteCallable]:
+        def route_decorator(func: RouteCallable) -> RouteCallable:
             enabled_middlewares = [
                 middleware for path_prefix, middleware in self.middlewares if
                 rule.lstrip("/").startswith(path_prefix)
